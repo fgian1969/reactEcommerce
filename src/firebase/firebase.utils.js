c9f5cc67e76cb4c8003dc1af0a9a1182f7eb5f02
Gian Carlo Ferrari <giancarlo.ferrari@siagroup.it>
2019-12-16 13:51:59 +0100

    aggiunto il form di registrazione nuovo utente

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,31 @@ const config = {
     appId: "1:964984021971:web:8c8b71974fbbc0a9244526"
   };
 
+export const createUserProfileDocument = async (userAuth, additionalData) =>{
+    if (!userAuth) return;
+    const userRef=firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+    if (!snapShot.exists){
+        const {displayName,email}=userAuth;
+        //console.log("OLE",displayName,email)
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        }
+        catch (error){
+            console.log('error creating user', error.message)
+        }
+       
+    }
+   
+    return userRef;
+}
+
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
